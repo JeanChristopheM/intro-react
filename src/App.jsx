efa96e7b3c8a604a2739e5e43f4c6f02bf8c88ac
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2021-11-05 13:50:45 +0100

    added delete done functionnality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import Input from './components/Input.jsx';
 import TodoList from './components/TodoList.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState(JSON.parse(localStorage.getItem('todoList')));
-  
-  const saveData = (data) => {
-    setData(data);
-    localStorage.setItem('todoList', JSON.stringify(data));
-  }
+  const [areThereDone, setAreThereDone] = useState(false);
 
   //Initialization of the localStorage
   if (!data) {
-    saveData([]);
+    setData([]);
   }
+  useEffect(() => {
+    window.localStorage.setItem('todoList', JSON.stringify(data));
+    let newData = data.filter(x => x.status === true);
+    newData.length > 0 ? setAreThereDone(true) : setAreThereDone(false);
+  }, [data]);
 
   // HANDLERS
   // Handling the adding of a new task
@@ -25,23 +26,27 @@ function App() {
     }
     let newData = [...data];
     newData.push(todo);
-    saveData(newData);
+    setData(newData);
   }
   // Handling the checking of a task
   const handleCheckmark = (id) => {
     let newData = [...data];
     let itemToUpdate = newData.find(element => element.id === id);
     itemToUpdate.status = !itemToUpdate.status;
-    saveData(newData);
+    setData(newData);
   }
   // Handling deletion of a task
   const handleDelete = (id) => {
     let newData = [...data];
     let index = newData.findIndex(todo => todo.id === id);
     newData.splice(index, 1);
-    saveData(newData);
+    setData(newData);
   }
 
+  const handleDeleteDone = () => {
+    let newData = data.filter(x => x.status === false);
+    setData(newData);
+  }
   return (
     <div className={"App"}>
       <header className={"header"}>
@@ -51,6 +56,9 @@ function App() {
         <Input handleAdd={handleAdd} />
         <TodoList data={data} handleCheckmark={handleCheckmark} handleDelete={handleDelete} />
       </main>
+      <footer className="footer">
+        {areThereDone && <button type="button" className={"deleteDone"} onClick={handleDeleteDone}>Delete done todos</button>}
+      </footer>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,13 +5,13 @@ const Input = ({handleAdd}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const inputElement = inputRef.current;
-        handleAdd(inputElement.value);
+        inputElement.value.length > 0 ? handleAdd(inputElement.value) : alert('Please enter a task');
         inputElement.value = '';
     }
     return (
         <form className={"main__input"} onSubmit={handleSubmit}>
             <input type="text" name="todoInput" id="todoInput" placeholder="Enter task here" ref={inputRef} />
-            <button className={"addBtn"} type="submit">Add todo</button>
+            <button className={"addBtn"} type="submit">Submit</button>
         </form>
     );
 }
